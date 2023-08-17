110709a056bbf4dcbfd0e2dfa54d8dc9e360c024
Andres Felipe Villarraga Arango <avillarraga@opc.com.co>
2023-08-17 17:46:46 -0500

    Se avanza con el model y la estructura basica de home

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -1,18 +1,23 @@
+import { Product } from "../../Model";
 
 
-export const Card = () =>{
+interface Props extends Omit<Product, 'id' | 'rating' > {}
+
+
+
+export const Card = ( { title,price,description,category,image }:Props ) =>{
     return (
         <div className="bg-white first-letter w-56 h-60 rounded">
             <figure className="relative mb-2 w-full h-4/8 ">
                 <div className="text-xs cursor-pointer absolute top-0 right-0 flex justify-center items-center bg-green-500 w-4 h-4 m-1 rounded-full">
                     +
                 </div>
-                <img className="w-full h-full object-cover rounded-t" src="https://www.muycomputer.com/wp-content/uploads/2022/12/iPhonePro14.jpg" alt="" />
+                <img className="w-full h-full object-cover rounded-t" src={image} alt={category} />
             </figure>
             <div className="relative">
-                <h3 className="text-xs font-bold">Title</h3>
-                <p className="text-xs">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-                <p className="flex justify-end"><span>$500</span></p>
+                <h3 className="text-xs font-bold">{title}</h3>
+                <p className="text-xs">{description}</p>
+                <p className="flex justify-end"><span>${price}</span></p>
             </div>
         </div>
     )
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react"
 import { Card, Layout } from "../../Components"
+import { Product } from "../../Model"
 
 export const Home = () => {
-    return (
-      <Layout>
-        <Card/>
-      </Layout>
-    )
-  }
+  const [products,setProducts] = useState<Product[]>([])
+
+  useEffect(() =>{
+    fetch('https://fakestoreapi.com/products?limit=5')
+      .then( res => res.json())
+      .then( (data:Product[]) => setProducts(data) )
+  },[])
+
+  return (
+    <Layout>
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        { products?.map( ({ id,title,price,description,category,image }) => (
+            <Card
+              key={id}
+              title={title}
+              price={price}
+              description={description}
+              category={category}
+              image={image}
+              />
+            )
+          )
+        }
+      </div>
+
+    </Layout>
+  )
+}
